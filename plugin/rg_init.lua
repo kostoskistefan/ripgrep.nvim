f3c4849578ebe814a720d811e16616137af22a1d
Kostoski Stefan <kostoski.stefan90@gmail.com>
2025-08-02 10:02:19 +0200

    Fix environment path separator

--- plugin/rg_init.lua
+++ plugin/rg_init.lua
@@ -6,6 +6,6 @@ if vim.fn.isdirectory(data_path) == 0 then
 end
 
 if vim.fn.executable('rg') == 0 then
-  local data_path = vim.fn.stdpath('data') .. '/ripgrep.nvim'
-  vim.env.PATH = vim.env.PATH .. ':' .. data_path
+  local env_path_separator = package.config:sub(1, 1) == '\\' and ';' or ':'
+  vim.env.PATH = vim.env.PATH .. env_path_separator .. data_path
 end
